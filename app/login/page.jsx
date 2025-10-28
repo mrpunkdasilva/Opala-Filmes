27f18a4b2f97d8ffb4e0416559383a2f10b5ed7f
Mr Punk da Silva <gustavojs417@gmail.com>
2025-10-28 12:41:02 -0300

    feat: Apply GlitchBackground to login page

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,8 +1,10 @@
 import LoginForm from "@/app/components/form/LoginForm";
+import GlitchBackground from "@/app/components/effects/glitch-background/GlitchBackground";
 
 export default function LoginPage() {
   return (
-    <main className="flex items-center justify-center min-h-screen bg-[var(--deep-space)]">
+    <main className="flex items-center justify-center min-h-screen">
+      <GlitchBackground />
       <LoginForm />
     </main>
   );
